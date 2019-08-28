567d9dd51e800550a4ef61044ab21d85101124f6
CarlosZiegler <carlos.ziegler@gmail.com>
2019-08-28 15:55:11 +0200

    Updates

---
+++ precache-manifest.4b5769c4d423a2a6988ce62fdb7a9d23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2879e95c6effafb44709e7c0dac9e4d",
+    "url": "/list-doctor/index.html"
+  },
+  {
+    "revision": "875026ec5be602195768",
+    "url": "/list-doctor/static/js/2.f255948d.chunk.js"
+  },
+  {
+    "revision": "4a765ee736bb14d561f1",
+    "url": "/list-doctor/static/js/main.c9068d76.chunk.js"
+  },
+  {
+    "revision": "16f990b21956e3688fbf",
+    "url": "/list-doctor/static/js/runtime~main.dac1b6d3.js"
+  },
+  {
+    "revision": "8d33bcfb47851581aca2f7a8f1086b37",
+    "url": "/list-doctor/static/media/data.8d33bcfb.csv"
+  }
+]);
